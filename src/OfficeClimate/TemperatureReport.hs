cb852a295eac7a6d932c1a81a7561b2622f9b7f5
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2015-04-07 01:21:37 +0900

    add db entities

---
+++ src/OfficeClimate/TemperatureReport.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE TemplateHaskell, MultiParamTypeClasses, FlexibleInstances #-}
+
+module OfficeClimate.TemperatureReport where
+
+import Database.HDBC.Query.TH (defineTableFromDB)
+import Database.HDBC.Schema.PostgreSQL (driverPostgreSQL)
+import Database.Record.TH (derivingShow)
+
+import OfficeClimate.Connection (connect)
+
+$(defineTableFromDB connect driverPostgreSQL "office_climate" "temperature_reports" [derivingShow])
---
+++ src/OfficeClimate/User.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE TemplateHaskell, MultiParamTypeClasses, FlexibleInstances #-}
+
+module OfficeClimate.User where
+
+import Database.HDBC.Query.TH (defineTableFromDB)
+import Database.HDBC.Schema.PostgreSQL (driverPostgreSQL)
+import Database.Record.TH (derivingShow)
+
+import OfficeClimate.Connection (connect)
+
+$(defineTableFromDB connect driverPostgreSQL "office_climate" "users" [derivingShow])
