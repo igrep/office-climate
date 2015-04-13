936c7d2252bbe9c639388b79b7e8fbf7cebfb01f
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2015-04-14 03:05:32 +0900

    add create user statement

--- src/OfficeClimate/User.hs
+++ src/OfficeClimate/User.hs
@@ -6,6 +6,11 @@ import Database.HDBC.Query.TH (defineTableFromDB)
 import Database.HDBC.Schema.PostgreSQL (driverPostgreSQL)
 import Database.Record.TH (derivingShow)
 
+import Database.Relational.Query (typedInsert, Insert)
+
 import OfficeClimate.Connection (connect)
 
 $(defineTableFromDB connect driverPostgreSQL "office_climate" "user" [derivingShow])
+
+create :: Insert String
+create = typedInsert tableOfUser name'
