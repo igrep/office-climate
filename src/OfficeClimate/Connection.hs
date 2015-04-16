8cd5fcf1977dd9f01c3472c00d9d8782422c6a95
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2015-04-17 00:20:01 +0900

    add function to run select statement
    but it seems not for insert statement

--- src/OfficeClimate/Connection.hs
+++ src/OfficeClimate/Connection.hs
@@ -1,8 +1,23 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module OfficeClimate.Connection
   ( connect
+  , run
   ) where
 
 import Database.HDBC.PostgreSQL (connectPostgreSQL, Connection)
+import Database.HDBC (IConnection, SqlValue)
+import Database.Record (FromSql, ToSql)
+import Database.HDBC.Record.Query (runQuery)
+import Database.Relational.Query (Relation, relationalQuery)
 
 connect :: IO Connection
 connect = connectPostgreSQL "dbname=office_climate"
+
+run :: (Show a, IConnection conn, FromSql SqlValue a, ToSql SqlValue p)
+       => conn -> p -> Relation p a -> IO ()
+run conn param rel = do
+  putStrLn $ "SQL: " ++ show rel
+  records <- runQuery conn (relationalQuery rel) param
+  mapM_ print records
+  putStrLn ""
