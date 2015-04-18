b55787f8446c56e5a7404ab74e7bd7aa31deae24
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2015-04-18 15:23:23 +0900

    reorder delete statement to avoid foreign key error

--- db/reset.sql
+++ db/reset.sql
@@ -2,6 +2,6 @@ ALTER SEQUENCE office_climate.user_user_id_seq RESTART WITH 1;
 ALTER SEQUENCE office_climate.place_place_id_seq RESTART WITH 1;
 ALTER SEQUENCE office_climate.temperature_report_temperature_report_id_seq RESTART WITH 1;
 
+delete from office_climate.temperature_report;
 delete from office_climate.user;
 delete from office_climate.place;
-delete from office_climate.temperature_report;
